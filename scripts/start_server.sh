f301b46c5ffb798aded30b5f8879b09979d432d8
DavidSanchezCertus <david.sanchez@rlbusiness.com.mx>
2024-06-28 13:18:27 -0700

    logica agregada

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,23 @@
 sudo fuser -k 5137/tcp || true
 # Iniciar la aplicación
 cd /home/ec2-user/dashboard
-sudo bash -c "PORT=5137 npm run dev" &
-exit 0
+# Obtener el PID del último comando en segundo plano
+APP_PID=$!
+TIMEOUT=60
+RETRY_INTERVAL=5
+elapsed=0
+while [[ $elapsed -lt $TIMEOUT ]]; do
+    if curl -s http://localhost:5137 > /dev/null; then
+        echo "Se inició la aplicación en el puerto 5137"
+        exit 0
+    else
+        echo "Esperando a que la aplicación se inicie..."
+        sleep $RETRY_INTERVAL
+        elapsed=$((elapsed + RETRY_INTERVAL))
+    fi
+done
+echo "Error: La aplicación falló en el puerto 5137"
+kill $APP_PID
+exit 1
+
+

25f1d2e2758c176e6ddb168c34a44b9e26314dec
DavidSanchezCertus <david.sanchez@rlbusiness.com.mx>
2024-06-28 13:03:26 -0700

    bash agregado

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,7 +3,7 @@
 sudo fuser -k 5137/tcp || true
 # Iniciar la aplicación
 cd /home/ec2-user/dashboard
-sudo nohup PORT=5137 npm run dev > /dev/null 2>&1 &
+sudo bash -c "PORT=5137 npm run dev" > /dev/null 2>&1 &
 #nohup npm run dev > /dev/null 2>&1 &
 sleep 40
 if curl -s http://localhost:5137 > /dev/null; then

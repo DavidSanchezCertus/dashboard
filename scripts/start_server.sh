cff6881e1b29b91b067ff1d23e6818a1d89766ce
DavidSanchezCertus <david.sanchez@rlbusiness.com.mx>
2024-06-28 12:26:52 -0700

    40 segundos start server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,7 +5,7 @@ sudo fuser -k 5137/tcp || true
 cd /home/ec2-user/dashboard
 nohup PORT=5137 npm run dev > /dev/null 2>&1 &
 #nohup npm run dev > /dev/null 2>&1 &
-sleep 10
+sleep 40
 if curl -s http://localhost:5137 > /dev/null; then
     echo "Se inicio la aplicación"
 else

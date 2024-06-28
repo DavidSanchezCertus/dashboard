8902e9c044cd773145e2ab7ebb1800342364df41
DavidSanchezCertus <david.sanchez@rlbusiness.com.mx>
2024-06-28 12:57:05 -0700

    timeout aumentado

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,7 +5,7 @@ sudo fuser -k 5137/tcp || true
 cd /home/ec2-user/dashboard
 sudo nohup PORT=5137 npm run dev > /dev/null 2>&1 &
 #nohup npm run dev > /dev/null 2>&1 &
-sleep 20
+sleep 40
 if curl -s http://localhost:5137 > /dev/null; then
     echo "Se inició la aplicación en el puerto 5137"
 else

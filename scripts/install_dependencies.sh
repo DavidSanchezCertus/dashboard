c1254221af0eea83099e2ae4e9569bd17f703185
DavidSanchezCertus <david.sanchez@rlbusiness.com.mx>
2024-06-28 13:00:06 -0700

    install codigo eliminado

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,6 +2,4 @@
 cd /home/ec2-user/dashboard
 sudo chown -R ec2-user:ec2-user /home/ec2-user/dashboard/*
 sudo chmod +x /home/ec2-user/dashboard/scripts/*
-npm cache clean --force
-rm -rf node_modules package-lock.json
 npm install

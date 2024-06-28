234cc37a3b511bfd83e3ed9fcc47ebe711d1f8cd
DavidSanchezCertus <david.sanchez@rlbusiness.com.mx>
2024-06-28 13:41:07 -0700

    puerto actualizado

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 # Detener cualquier proceso que esté usando el puerto 3000
-sudo fuser -k 5137/tcp || true
+sudo fuser -k 5173/tcp || true
 # Iniciar la aplicación
 cd /home/ec2-user/dashboard
-sudo bash -c "PORT=5137 npm run dev" &
+sudo bash -c "npm run dev" &
 # Esperar unos segundos para permitir que la aplicación se inicie
 sleep 10
-# Verificar si la aplicación se inició correctamente
-if curl -s http://localhost:5137 > /dev/null; then
-    echo "Se inició la aplicación en el puerto 5137"
-    exit 0
-else
-    echo "Error: La aplicación falló en el puerto 5137"
-    exit 1
-fi
+echo "Aplicacion iniciada"
+exit 0
--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sleep 5
-curl -f http://localhost:5137 || exit 1
+curl -f http://localhost:5173 || exit 1
 echo "La aplicación esta corriendo correctamente"
